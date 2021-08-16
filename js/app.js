de3fabab862f40e03d1c4e3e4c0373434afb11a8
gili1010 <lucianog_332@hotmail.com>
2021-08-16 01:06:12 -0300

    desafio5 objetos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,55 @@
-let edad = prompt("ingrese su edad");
-let nombre = prompt("ingrese su nombre");
-let precio = 0;
+/* obtenemos los datos de lapersona */
 
+class Persona {
+     constructor(nombrePersona, edad, localidad){
+          this.nombrePersona = nombrePersona;
+          this.edad = edad;
+          this.localidad = localidad;
+
+     }
+     presentacion(){
+          console.log(`Bienvenido ${this.nombrePersona} su edad es ${this.edad} y vive en ${this.localidad}`);
+     }
+
+}
+
+/* lapersona carga los datos por pantalla */
+const persona1 = new Persona (nombre = prompt("ingrese su nombre"),edad = parseInt(prompt("ingrese su edad")),prompt("ingrese su localidad"));
+
+/* mostramos x consola la persona */
+console.log(persona1);
+persona1.presentacion();
+
+
+/* obtenemos los datos del producto */
+
+class Producto {
+     constructor(descripcion, precio, cantPedida){
+          this.descripcion = descripcion;
+          this.precio = precio;
+          this.cantidadPedida = cantPedida;
+     }
+     descripcionProducto(){
+          console.log(`Su producto es ${this.descripcion} y la cantidad pedida es ${this.cantidadPedida}`);
+     }
+};
+
+let precio = 0; 
+
+/* ingresamos los datos por consola */
+const producto1 = new Producto(producto = prompt("ingrese producto a comprar:  \n -vino \n -cerveza \n -gaseosa"),parseFloat(precio),cantidad = parseInt(prompt("ingrese cantidad")));
+
+/* mostramos los datos del producto */
+console.log(producto1);
+producto1.descripcionProducto();
+
+
+
+/* consultamos si es mayor de edad */
 if (edad >= 18) {
      alert("Bienvenido " + nombre);
 
-     let producto = prompt("ingrese producto a comprar:  \n -vino \n -cerveza \n -gaseosa");
+     /* si es mayor de edad se ejecuta el siguiente codigo preguntando que va a comprar para sacar su precio */
 
      switch (producto) {
           case "vino":
@@ -23,9 +67,43 @@ if (edad >= 18) {
                break;
      }; 
 
-
+/* si no es mayor de edad */
 } else {
-     alert("Lo sentimos " + nombre + ' ' + "pero necesita ser mayor de 18 años para comprar");
+     alert(`Lo sentimos ${nombre} pero necesita ser mayor de 18 años para comprar`);
 }
 
+/* obtuvimos el precio */
 console.log(precio);
+
+/* depende la cantidad solicitada es el descuento que se le va a realizar por la compra */
+descuento= 0;
+
+if (cantidad <= 1){
+     descuento = 5;
+} else if (descuento >=2 && descuento <5) {
+     descuento = 10;
+}else{
+     descuento = 20;
+}
+
+const suma = (a,b) => a+b;
+const resta = (a,b) => a-b;
+
+/* funcion para calcular el iva */
+const iva = x => x * 0.21;
+
+
+/* calculamos y mostramos el precio total de la compra con el iva y el descuento realizado */
+
+const precioTotal = () => {
+     let total =resta(suma(precio, iva(precio)), descuento);
+     return total;
+}
+
+/* total = (precio + iva(precio)) - descuento; */
+
+const resultado = () =>{
+     console.log ("el precio total a pagar es: " + precioTotal());
+}
+
+resultado();
